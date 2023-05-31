1b9b66fbc0cb7820950859612539be5e8abda6ce
LZ-QWQ <lz1830837889@163.com>
2023-05-31 14:44:05 +0800

    update

--- test.sh
+++ test.sh
@@ -15,7 +15,7 @@
 # norm test
 CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" OMP_NUM_THREADS=1 MKL_NUM_THREADS=1 \
 bash ./tools/dist_test.sh ./configs/bevdet_occ/bevdet-occ-intenimage_B_custom_decay-4d-stereo-672x1408-24e-labelsmooth_0.0001-load-CBGS.py \
- ./ckpts/bevdet-InternImage-672*1408-epoch_7_ema.pth 8 --eval miao
+ ./ckpts/bevdet-InternImage-672*1408-epoch_9_ema.pth 8 --eval miao
 
 # TTA test very slow !!!!!!
 # CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" OMP_NUM_THREADS=1 MKL_NUM_THREADS=1 \
