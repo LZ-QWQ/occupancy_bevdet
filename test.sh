22eb0620793b01e5588d13fbd35b294b0380ad97
LZ-QWQ <ftgreat@163.com>
2023-05-31 06:57:30 +0000

    update

--- test.sh
+++ test.sh
@@ -12,10 +12,17 @@
 
 #  | tee -a ./work_dirs/test_log_internimage_B-LB_0.00001-load.txt
 
-# norm test
+# ============================= norm test ==========================================
+
+# CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" OMP_NUM_THREADS=1 MKL_NUM_THREADS=1 \
+# bash ./tools/dist_test.sh ./configs/bevdet_occ/bevdet-occ-intenimage_B_custom_decay-4d-stereo-672x1408-24e-labelsmooth_0.0001-load-CBGS.py \
+#  ./ckpts/bevdet-InternImage-672*1408-epoch_9_ema.pth 8 --eval miao
+
 CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" OMP_NUM_THREADS=1 MKL_NUM_THREADS=1 \
 bash ./tools/dist_test.sh ./configs/bevdet_occ/bevdet-occ-intenimage_B_custom_decay-4d-stereo-672x1408-24e-labelsmooth_0.0001-load-CBGS.py \
- ./ckpts/bevdet-InternImage-672*1408-epoch_9_ema.pth 8 --eval miao
+ /data/work_dirs/bevdet-occ-intenimage_B_custom_decay-4d-stereo-672x1408-24e-labelsmooth_0.0001-load-CBGS/epoch_9_ema.pth 8 --eval miao
+
+# ==================================================================================
 
 # TTA test very slow !!!!!!
 # CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" OMP_NUM_THREADS=1 MKL_NUM_THREADS=1 \
